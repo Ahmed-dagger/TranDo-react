3c72e1696d370f3306fed6fff997b7f25358825c
Ahmed dagger <ahdbo124@gmail.com>
2023-09-16 18:24:46 +0300

    fixes

--- components/footer.jsx
+++ components/footer.jsx
@@ -4,14 +4,9 @@ import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBIcon } from 'mdb-react-ui-k
 const Footer = () => {
   return (
 
-    <MDBFooter bgColor='dark' className='text-center text-lg-left'>
-      <div className='text-center text-light p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-        &copy; {new Date().getFullYear()} Copyright:{' '}
-        <a className='text-light' href='#'>
-          TranDo.github.io
-        </a>
-      </div>
-    </MDBFooter>
+    <footer className='footer'>
+      <p>Copyright © TranDo 2023</p>
+    </footer>
    
   )
 }
